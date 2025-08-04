a6931fe0c94e3b43b0560334e248f4909c92eb84
koriebruh <koriebruh@gmail.com>
2025-08-05 02:24:57 +0700

    yay

--- BE/src/main/java/com/koriebruh/be/dto/LoginRequest.java
+++ BE/src/main/java/com/koriebruh/be/dto/LoginRequest.java
@@ -13,12 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginRequest {
 
-    @NotBlank(message = "can't be blank")
-    @Size(min = 1, max = 70, message = "length name around 1 - 70 digit")
     private String username;
 
-    @NotBlank(message = "password can't be blank")
-    @Size(min = 8, max = 70, message = "length password around 8 - 70 digit")
     private String password;
 
 }
